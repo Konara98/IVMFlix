0366191f0b0675376b66c01875e83a16370fde12
Konara98 <lakshithakonara3@gmail.com>
2024-01-05 23:51:50 +0530

    Adjusted the indentations

--- Controllers/moviesController.js
+++ Controllers/moviesController.js
@@ -10,35 +10,35 @@ exports.getHighestRatedMovies = (req, res, next) => {
 }
 
 exports.getAllMovies = asyncErrorHandler(async (req, res, next) => {
-        let features = new ApiFeatures(Movie.find(), req.query, ['sort', 'fields', 'page', 'limit'])
+    let features = new ApiFeatures(Movie.find(), req.query, ['sort', 'fields', 'page', 'limit'])
                                             .filter();                                                                      //Object to find the movie count and add it to below object
-        const moviesCount = await features.query.countDocuments();
+    const moviesCount = await features.query.countDocuments();
 
-        features = new ApiFeatures(Movie.find(), req.query, ['sort', 'fields', 'page', 'limit'], moviesCount)
+    features = new ApiFeatures(Movie.find(), req.query, ['sort', 'fields', 'page', 'limit'], moviesCount)
                                             .filter()
                                             .sort()
                                             .limitFields()
                                             .paginate();
-        const movies = await features.query;
+    const movies = await features.query;
 
-        res.status(200).json({
-            status: 'success',
-            length: movies.length,
-            data: {
-                movies
-            }
-        })
+    res.status(200).json({
+        status: 'success',
+        length: movies.length,
+        data: {
+            movies
+        }
+    })
 });
 
 exports.getMovie = asyncErrorHandler(async (req, res, next) => {
-        const movie = await Movie.find({name: req.params.name});
+    const movie = await Movie.find({name: req.params.name});
 
-        res.status(200).json({
-            status: 'success',
-            data: {
-                movie
-            }
-        })
+    res.status(200).json({
+        status: 'success',
+        data: {
+            movie
+        }
+    })
 });
 
 exports.createMovie = asyncErrorHandler(async (req, res, next) => {
@@ -53,97 +53,97 @@ exports.createMovie = asyncErrorHandler(async (req, res, next) => {
 });
 
 exports.updateMovie = asyncErrorHandler(async (req, res, next) => {
-        const updatedMovie = await Movie.findOneAndUpdate(req.params, req.body, {new: true, runValidators: true});
+    const updatedMovie = await Movie.findOneAndUpdate(req.params, req.body, {new: true, runValidators: true});
 
-        res.status(200).json({
-            status: 'success',
-            data: {
-                updatedMovie
-            }
-        })
+    res.status(200).json({
+        status: 'success',
+        data: {
+            updatedMovie
+        }
+    })
 });
 
 exports.deleteMovie = asyncErrorHandler(async (req, res, next) => {
-        await Movie.findOneAndDelete(req.params);
+    await Movie.findOneAndDelete(req.params);
 
-        res.status(204).json({
-            status: 'success',
-            data: null
-        })
+    res.status(204).json({
+        status: 'success',
+        data: null
+    })
 });
 
 exports.getMovieStats = asyncErrorHandler(async (req, res, next) => {
-        const stats = await Movie.aggregate([
-            {$match: {ratings: {$gte: 4.5}}},
-            {$group: {
-                _id: '$releaseYear',            //group on this field
-                avgRating: {$avg: '$ratings'},
-                avgPrice: {$avg: '$price'},
-                minPrice: {$min: '$price'},
-                maxPrice: {$max: '$price'},
-                priceTotal: {$sum: '$price'},
-                moviCount: {$sum: 1}
-            }},
-            {$sort: {minPrice: 1}},
-            // {$match: {maxPrice: {$gte: 60}}}
-        ]);
+    const stats = await Movie.aggregate([
+        {$match: {ratings: {$gte: 4.5}}},
+        {$group: {
+            _id: '$releaseYear',            //group on this field
+            avgRating: {$avg: '$ratings'},
+            avgPrice: {$avg: '$price'},
+            minPrice: {$min: '$price'},
+            maxPrice: {$max: '$price'},
+            priceTotal: {$sum: '$price'},
+            moviCount: {$sum: 1}
+        }},
+        {$sort: {minPrice: 1}},
+        // {$match: {maxPrice: {$gte: 60}}}
+    ]);
 
-        res.status(200).json({
-            status: 'success',
-            length: stats.length,
-            data: {
-                stats
-            }
-        })
+    res.status(200).json({
+        status: 'success',
+        length: stats.length,
+        data: {
+            stats
+        }
+    })
 });
 
 exports.getMovieByGenre = asyncErrorHandler(async (req, res, next) => {
-        const genre = req.params.genre;
-        const movies = await Movie.aggregate([
-            {$unwind: '$genres'},
-            {$group: {
-                _id: '$genres',
-                movieCount: {$sum: 1},
-                movies: {$push: '$name'},
+    const genre = req.params.genre;
+    const movies = await Movie.aggregate([
+        {$unwind: '$genres'},
+        {$group: {
+            _id: '$genres',
+            movieCount: {$sum: 1},
+            movies: {$push: '$name'},
 
-            }},
-            {$addFields: {genre: '$_id'}},
-            {$project: {_id: 0}},
-            {$sort: {movieCount: -1}},
-            // {$limit: 6},
-            {$match: {genre: genre}}
-        ]);
+        }},
+        {$addFields: {genre: '$_id'}},
+        {$project: {_id: 0}},
+        {$sort: {movieCount: -1}},
+        // {$limit: 6},
+        {$match: {genre: genre}}
+    ]);
 
-        res.status(200).json({
-            status: 'success',
-            length: movies.length,
-            data: {
-                movies
-            }
-        })
+    res.status(200).json({
+        status: 'success',
+        length: movies.length,
+        data: {
+            movies
+        }
+    })
 });
 
 exports.getMovieByDirector = asyncErrorHandler(async (req, res, next) => {
-        const director = req.params.director;
-        const movies = await Movie.aggregate([
-            {$unwind: '$directors'},
-            {$group: {
-                _id: '$directors',
-                movieCount: {$sum: 1},
-                movies: {$push: '$name'},
+    const director = req.params.director;
+    const movies = await Movie.aggregate([
+        {$unwind: '$directors'},
+        {$group: {
+            _id: '$directors',
+            movieCount: {$sum: 1},
+            movies: {$push: '$name'},
 
-            }},
-            {$addFields: {director: '$_id'}},
-            {$project: {_id: 0}},
-            {$sort: {movieCount: -1}},
-            {$match: {director: director}}
-        ]);
+        }},
+        {$addFields: {director: '$_id'}},
+        {$project: {_id: 0}},
+        {$sort: {movieCount: -1}},
+        {$match: {director: director}}
+    ]);
 
-        res.status(200).json({
-            status: 'success',
-            length: movies.length,
-            data: {
-                movies
-            }
-        })
+    res.status(200).json({
+        status: 'success',
+        length: movies.length,
+        data: {
+            movies
+        }
+    })
 });
--- Controllers/videosController.js
+++ Controllers/videosController.js
@@ -10,114 +10,114 @@ exports.getHighestRatedVideos = (req, res, next) => {
 }
 
 exports.getAllVideos = asyncErrorHandler(async (req, res, next) => {
-        let features = new ApiFeatures(Video.find(), req.query, ['sort', 'fields', 'page', 'limit'])
-                                            .filter();                                                                      //Object to find the video count and add it to below object
-        const videosCount = await features.query.countDocuments();
+    let features = new ApiFeatures(Video.find(), req.query, ['sort', 'fields', 'page', 'limit'])
+                                        .filter();                                                                      //Object to find the video count and add it to below object
+    const videosCount = await features.query.countDocuments();
 
-        features = new ApiFeatures(Video.find(), req.query, ['sort', 'fields', 'page', 'limit'], videosCount)
-                                            .filter()
-                                            .sort()
-                                            .limitFields()
-                                            .paginate();
-        const videos = await features.query;
+    features = new ApiFeatures(Video.find(), req.query, ['sort', 'fields', 'page', 'limit'], videosCount)
+                                        .filter()
+                                        .sort()
+                                        .limitFields()
+                                        .paginate();
+    const videos = await features.query;
 
-        res.status(200).json({
-            status: 'success',
-            length: videos.length,
-            data: {
-                videos
-            }
-        })
+    res.status(200).json({
+        status: 'success',
+        length: videos.length,
+        data: {
+            videos
+        }
+    })
 });
 
 exports.getVideo = asyncErrorHandler(async (req, res, next) => {
-        const video = await Video.find(req.params);
+    const video = await Video.find(req.params);
 
-        res.status(200).json({
-            status: 'success',
-            data: {
-                video
-            }
-        })
+    res.status(200).json({
+        status: 'success',
+        data: {
+            video
+        }
+    })
 });
 
 exports.createVideo = asyncErrorHandler(async (req, res) => {
-        const newVideo = await Video.create(req.body);
-    
-        res.status(201).json({
-            status: 'success',
-            data: {
-                newVideo
-            }
-        })
+    const newVideo = await Video.create(req.body);
+
+    res.status(201).json({
+        status: 'success',
+        data: {
+            newVideo
+        }
+    })
 });
 
 exports.updateVideo = asyncErrorHandler(async (req, res) => {
-        const updatedVideo = await Video.findOneAndUpdate(req.params, req.body, {new: true, runValidators: true});
+    const updatedVideo = await Video.findOneAndUpdate(req.params, req.body, {new: true, runValidators: true});
 
-        res.status(200).json({
-            status: 'success',
-            data: {
-                updatedVideo
-            }
-        })
+    res.status(200).json({
+        status: 'success',
+        data: {
+            updatedVideo
+        }
+    })
 });
 
 exports.deleteVideo = asyncErrorHandler(async (req, res) => {
-        await Video.findOneAndDelete(req.params);
+    await Video.findOneAndDelete(req.params);
 
-        res.status(204).json({
-            status: 'success',
-            data: null
-        })
+    res.status(204).json({
+        status: 'success',
+        data: null
+    })
 });
 
 exports.getVideoByGenre = asyncErrorHandler(async (req, res) => {
-        const genre = req.params.genre;
-        const videos = await Video.aggregate([
-            {$unwind: '$genres'},
-            {$group: {
-                _id: '$genres',
-                videoCount: {$sum: 1},
-                videos: {$push: '$name'},
+    const genre = req.params.genre;
+    const videos = await Video.aggregate([
+        {$unwind: '$genres'},
+        {$group: {
+            _id: '$genres',
+            videoCount: {$sum: 1},
+            videos: {$push: '$name'},
 
-            }},
-            {$addFields: {genre: '$_id'}},
-            {$project: {_id: 0}},
-            {$sort: {videoCount: -1}},
-            {$match: {genre: genre}}
-        ]);
+        }},
+        {$addFields: {genre: '$_id'}},
+        {$project: {_id: 0}},
+        {$sort: {videoCount: -1}},
+        {$match: {genre: genre}}
+    ]);
 
-        res.status(200).json({
-            status: 'success',
-            length: videos.length,
-            data: {
-                videos
-            }
-        })
+    res.status(200).json({
+        status: 'success',
+        length: videos.length,
+        data: {
+            videos
+        }
+    })
 });
 
 exports.getVideoByDirector = asyncErrorHandler(async (req, res) => {
-        const director = req.params.director;
-        const videos = await Video.aggregate([
-            {$unwind: '$directors'},
-            {$group: {
-                _id: '$directors',
-                videoCount: {$sum: 1},
-                videos: {$push: '$name'},
+    const director = req.params.director;
+    const videos = await Video.aggregate([
+        {$unwind: '$directors'},
+        {$group: {
+            _id: '$directors',
+            videoCount: {$sum: 1},
+            videos: {$push: '$name'},
 
-            }},
-            {$addFields: {director: '$_id'}},
-            {$project: {_id: 0}},
-            {$sort: {videoCount: -1}},
-            {$match: {director: director}}
-        ]);
+        }},
+        {$addFields: {director: '$_id'}},
+        {$project: {_id: 0}},
+        {$sort: {videoCount: -1}},
+        {$match: {director: director}}
+    ]);
 
-        res.status(200).json({
-            status: 'success',
-            length: videos.length,
-            data: {
-                videos
-            }
-        })
+    res.status(200).json({
+        status: 'success',
+        length: videos.length,
+        data: {
+            videos
+        }
+    })
 });
