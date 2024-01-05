70151808f430443786e7ca58b6898a065dc614c5
Konara98 <lakshithakonara3@gmail.com>
2024-01-06 01:27:33 +0530

    Handled development and production error

--- Controllers/errorController.js
+++ Controllers/errorController.js
@@ -1,8 +1,33 @@
+const devErrors = (res, error) => {
+    res.status(error.statusCode).json({
+        status: error.status,
+        message: error.message,
+        stackTrace: error.stackTrace,
+        error: error
+    });
+}
+
+const prodErrors = (res, error) => {
+    if(error.isOperational){
+        res.status(error.statusCode).json({
+            status: error.status,
+            message: error.message
+        });
+    } else{
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went wrong! please try again later!'
+        });
+    }
+}
+
 exports.globalErrorHandler = (error, req, res, next) => {        //global error handling
     error.statusCode = error.statusCode || 500;
     error.status = error.status || 'error';
-    res.status(error.statusCode).json({
-        status: error.status,
-        message: error.message
-    });
+
+    if(process.env.NODE_ENV == 'development'){
+        devErrors(res, error);
+    } else if (process.env.NODE_ENV == 'production'){
+        prodErrors(res, error);
+    }
 }
