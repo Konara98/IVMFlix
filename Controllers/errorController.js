dd21373fc7aaf59db662309c0b92fe534414edd9
Konara98 <lakshithakonara3@gmail.com>
2024-01-06 14:39:45 +0530

    Handled duplicate key error

--- Controllers/errorController.js
+++ Controllers/errorController.js
@@ -1,8 +1,10 @@
+const CustomError = require('./../Utils/CustomError');
+
 const devErrors = (res, error) => {
     res.status(error.statusCode).json({
         status: error.status,
         message: error.message,
-        stackTrace: error.stackTrace,
+        stackTrace: error.stack,
         error: error
     });
 }
@@ -21,6 +23,12 @@ const prodErrors = (res, error) => {
     }
 }
 
+const duplicateKeyErrorHandler = (err) => {
+    const name = err.keyValue.name;
+    const msg = `There is already a movie with name ${name}. please use another name`;
+    return new CustomError(msg, 400); 
+}
+
 exports.globalErrorHandler = (error, req, res, next) => {        //global error handling
     error.statusCode = error.statusCode || 500;
     error.status = error.status || 'error';
@@ -28,6 +36,9 @@ exports.globalErrorHandler = (error, req, res, next) => {        //global error 
     if(process.env.NODE_ENV == 'development'){
         devErrors(res, error);
     } else if (process.env.NODE_ENV == 'production'){
+        if(error.code == 11000){
+            error = duplicateKeyErrorHandler(error);
+        } 
         prodErrors(res, error);
     }
 }
