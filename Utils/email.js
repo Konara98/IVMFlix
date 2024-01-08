625c2d4971d5700480e90202946d04da9d3a146b
Konara98 <lakshithakonara3@gmail.com>
2024-01-08 12:25:28 +0530

    Added gmail service to sens emails

--- Utils/email.js
+++ Utils/email.js
@@ -3,8 +3,7 @@ const nodemailer = require('nodemailer');
 const sendEmail = async (option) => {
     //Create a transporter
     const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
+        service: 'Gmail',
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASSWORD
